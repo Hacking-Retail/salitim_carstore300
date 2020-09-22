b7ce52409271ec608db5345786533271189e2816
Said <s.lotus69@gmail.com>
2020-09-22 13:03:33 +0200

    feat : adding some test on test file

--- test.py
+++ test.py
@@ -17,12 +17,21 @@ class TestGlobal(unittest.TestCase):
     def test_home(self):
         res = self.client().get('/')
         self.assertEqual(res.data, b'Welcome buddy')
-    # Basic authentication with 64bit encode of (user and password):
+
+    # Basic authentication is needed for test with 64bit encode of (user and password):
     def test_response_cars(self):
         res = self.client().get('/cars', headers={
                 "Authorization": "Basic dXNlcmJldGExOnRlc3Q="})
-        self.assertEqual(res.success, 200)
+        print(json.loads(res.data)['success'])
+        self.assertEqual(json.loads(res.data)['success'], True)
 
+        # For Test Basic authentication is needed with 64bit encode of (user and password):
+
+    def test_response_one_car(self):
+        res = self.client().get('/cars/2', headers={
+            "Authorization": "Basic dXNlcmJldGExOnRlc3Q="})
+        print(json.loads(res.data)['success'])
+        self.assertEqual(json.loads(res.data)['success'], True)
 
 if __name__ == '__main__':
     unittest.main()
