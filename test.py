cd6b0860b1354e9d0b1620cc054980c793c1c604
Said <s.lotus69@gmail.com>
2020-09-22 12:52:55 +0200

    feat: adding some test on postman file for testing route and cars type object

--- test.py
+++ test.py
@@ -3,6 +3,9 @@
 # from flask_cors import CORS, cross_origin
 from app.app import create_app
 import unittest
+import base64
+import json
+import os
 
 
 class TestGlobal(unittest.TestCase):
@@ -11,9 +14,15 @@ class TestGlobal(unittest.TestCase):
         self.app = create_app()
         self.client = self.app.test_client
 
-    def test_response_home(self):
+    def test_home(self):
         res = self.client().get('/')
-        self.assertEqual(res.statut_code, 200)
+        self.assertEqual(res.data, b'Welcome buddy')
+    # Basic authentication with 64bit encode of (user and password):
+    def test_response_cars(self):
+        res = self.client().get('/cars', headers={
+                "Authorization": "Basic dXNlcmJldGExOnRlc3Q="})
+        self.assertEqual(res.success, 200)
+
 
 if __name__ == '__main__':
     unittest.main()
